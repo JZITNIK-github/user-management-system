51d65ec691adb0000847c617f5e22f271a0b72f3
JZITNIK-github <kintiz@seznam.cz>
2023-10-18 08:03:18 +0200

    update factory reset script

--- factory.js
+++ factory.js
@@ -1,15 +1,18 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-
-(async ()=>{
-  await fs.writeFileSync("./users.json", JSON.stringify([
-    {
-      username: "admin",
-      password: "",
-      admin: true,
-    },
-  ]))
-  await fs.writeFileSync("./tokens.json", JSON.stringify({}))
-  await fs.writeFileSync("./config.json", JSON.stringify({port: 1111}))
-  console.log("Resetováno do továrního nastavení")
-})()
+(async () => {
+  try {
+    await fs.writeFile("./users.json", JSON.stringify([
+      {
+        username: "admin",
+        password: "",
+        admin: true,
+      },
+    ]));
+    await fs.writeFile("./tokens.json", JSON.stringify({}));
+    await fs.writeFile("./config.json", JSON.stringify({ port: 1111 }));
+    console.log("Resetováno do továrního nastavení");
+  } catch (error) {
+    console.error("Nastala chyba při resetování do továrního nastavení:", error);
+  }
+})();
