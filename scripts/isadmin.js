34694dca12fde903ca814b39cac50bc8341e5fea
JZITNIK-github <kintiz@seznam.cz>
2023-10-23 14:47:54 +0200

    fix bug

--- scripts/isadmin.js
+++ scripts/isadmin.js
@@ -1,3 +1,10 @@
+const fs = require("fs")
+const path = require("path")
+const paths = {
+    token: "./data/tokens.json",
+    users: "./data/users.json",
+    notfound: path.join(__dirname, 'public', '404.html')
+}
 module.exports = (token, callback) => {
     fs.readFile(paths.token, "utf-8", (err, data) => {
         if (err) {
